378581f194fb8b759a4a4bca1f7a454818f356ac
blutarche <aikdanai.s@gmail.com>
2026-04-10 21:43:36 +0700

    remove static plugins

--- zsh/config/plugins.zsh
+++ zsh/config/plugins.zsh
@@ -1,12 +1,3 @@
-fpath+=( "$HOME/Library/Caches/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git" )
-source "$HOME/Library/Caches/antidote/github.com/ohmyzsh/ohmyzsh/plugins/git/git.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/ohmyzsh/ohmyzsh/plugins/docker" )
-source "$HOME/Library/Caches/antidote/github.com/ohmyzsh/ohmyzsh/plugins/docker/docker.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/ohmyzsh/ohmyzsh/plugins/command-not-found" )
-source "$HOME/Library/Caches/antidote/github.com/ohmyzsh/ohmyzsh/plugins/command-not-found/command-not-found.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-syntax-highlighting" )
-source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-autosuggestions" )
-source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh"
-fpath+=( "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-history-substring-search" )
-source "$HOME/Library/Caches/antidote/github.com/zsh-users/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh"
+# Plugins are loaded by antidote from plugins.txt (see .zshrc).
+# Do not add static source lines to Antidote cache paths here — they fail when
+# the cache is empty (fresh machine, cleared ~/Library/Caches/antidote, etc.).
